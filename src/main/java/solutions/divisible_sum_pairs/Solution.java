ec98edd3c4ba374492bfee3880aeecebbf39b5c3
Sahinogullari <mehmetalisahinogullari@hotmail.com>
2019-04-15 21:23:04 +0300

    Divisible Sum Pairs

--- src/main/java/solutions/divisible_sum_pairs/Solution.java
+++ src/main/java/solutions/divisible_sum_pairs/Solution.java
@@ -11,48 +11,33 @@ import java.io.IOException;
 
 public class Solution {
 
-    // Complete the divisibleSumPairs function below.
-    static int divisibleSumPairs(int n, int k, int[] ar) {
-        int countOfSuitValues = 0;
-        for (int i = 0; i < ar.length; i++) {
-            for (int j = 0; j < ar.length; j++) {
-                if (j > i && isDivisible(new int[]{ar[i], ar[j]}, k))
-                    countOfSuitValues += 1;
-            }
-        }
-        return countOfSuitValues;
+  // Complete the divisibleSumPairs function below.
+  static int divisibleSumPairs(int n, int k, int[] ar) {
+    int countOfSuitValues = 0;
+    for (int i = 0; i < ar.length; i++) {
+      for (int j = i; j < ar.length; j++) {
+        if (j > i && isDivisible(new int[]{ar[i], ar[j]}, k))
+          countOfSuitValues += 1;
+      }
     }
+    return countOfSuitValues;
+  }
 
-    private static boolean isDivisible(int[] pair, int value) {
-        int total = pair[0] + pair[1];
-        return total > value
-                ? total % value == 0
-                : value % total == 0;
-    }
+  private static boolean isDivisible(int[] pair, int value) {
+    int total = pair[0] + pair[1];
+    return total % value == 0;
+  }
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        int n = Util.getPositiveIntValue(100);
-        n = 100;
+    int n = Util.getPositiveIntValue(100);
 
-        int k = Util.getPositiveIntValue(100);
-        k = 22;
+    int k = Util.getPositiveIntValue(100);
 
-        int[] ar = Util.getIntArray(100, n);
-        ar = new int[]{
-                43, 95, 51, 55, 40, 86, 65, 81, 51, 20,
-                47, 50, 65, 53, 23, 78, 75, 75, 47, 73,
-                25, 27, 14, 8, 26, 58, 95, 28, 3, 23,
-                48, 69, 26, 3, 73, 52, 34, 7, 40, 33,
-                56, 98, 71, 29, 70, 71, 28, 12, 18, 49,
-                19, 25, 2, 18, 15, 41, 51, 42, 46, 19,
-                98, 56, 54, 98, 72, 25, 16, 49, 34, 99,
-                48, 93, 64, 44, 50, 91, 44, 17, 63, 27,
-                3, 65, 75, 19, 68, 30, 43, 37, 72, 54,
-                82, 92, 37, 52, 72, 62, 3, 88, 82, 71};
+    int[] ar = Util.getIntArray(100, n);
 
-        int result = divisibleSumPairs(n, k, ar);
+    int result = divisibleSumPairs(n, k, ar);
+    System.out.println(result);
 
-
-    }
+  }
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -8,20 +10,40 @@ import java.util.Random;
  */
 public class Util {
 
-    public static int getPositiveIntValue(int maxValue) {
-        int estimate = Util.getIntValue(maxValue);
-        return estimate < 0 ? estimate * -1 : estimate;
-    }
+  public static int getPositiveIntValue(int maxValue) {
+    int estimate = Util.getIntValue(maxValue);
+    return estimate < 0 ? estimate * -1 : estimate;
+  }
 
-    public static int getIntValue(int maxValue) {
-        return (new Random()).nextInt() % maxValue + 1;
-    }
+  public static int getIntValue(int maxValue) {
+    return new Random().nextInt() % maxValue + 1;
+  }
 
-    public static int[] getIntArray(int maxValue, int lengthOfArray) {
-        int[] array = new int[lengthOfArray];
-        for (int i = 0; i < lengthOfArray; i++) {
-            array[i] = Util.getPositiveIntValue(maxValue);
-        }
-        return array;
+  public static int getIntValue(int min, int max) {
+    return new Random().nextInt(max - min) + min;
+  }
+
+  public static List<Integer> getIntList(int min, int max, int length) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < length; i++) {
+      list.add(getIntValue(min, max));
     }
+    return list;
+  }
+
+  public static int[] getIntArray(int min, int max, int length) {
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++) {
+      array[i] = getIntValue(min, max);
+    }
+    return array;
+  }
+
+  public static int[] getIntArray(int maxValue, int lengthOfArray) {
+    int[] array = new int[lengthOfArray];
+    for (int i = 0; i < lengthOfArray; i++) {
+      array[i] = Util.getPositiveIntValue(maxValue);
+    }
+    return array;
+  }
 }
